544ce3283de1e9178d70cc768885b8c362d312fc
changwenlong1 <513770370@qq.com>
2020-11-20 18:04:48 +0800

    新建项目

--- src/main/java/com/example/order/AgelectricalApplication.java
+++ src/main/java/com/example/order/AgelectricalApplication.java
@@ -1,9 +1,11 @@
 package com.example.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.order.mapper")
 public class AgelectricalApplication {
 
     public static void main(String[] args) {
